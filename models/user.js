9ad1528dbf67d0fee62f2a3c04e2cc3ca160aec9
Anson Tu <anson9137@gmail.com>
2019-11-23 15:09:25 -0500

    Added hashing for passwords

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt-nodejs");
 
 const userSchema = Schema({
   // TODO: Allergens, calorie count,
@@ -8,5 +9,30 @@ const userSchema = Schema({
   name: String,
   recipeIds: Object
 });
+
+userSchema.pre("save", function(next) {
+  const user = this;
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, isMatch);
+  });
+};
 
 module.exports = mongoose.model("user", userSchema);
