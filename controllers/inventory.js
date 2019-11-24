c5df07af1311e5c5f80129785ff1a08a2496d8f7
Anson Tu <anson9137@gmail.com>
2019-11-24 06:00:31 -0500

    Fixed recipe collection and created test route

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -1,7 +1,8 @@
-const Inventory = require("../models/inventory");
+const User = require("../models/user");
+const Recipe = require("../models/recipe");
 
 exports.getInventory = (req, res, next) => {
-  User.findOne({ userId: req.userId }, function(err, existingUser) {
+  User.findOne({ _id: req.user._id }, function(err, existingUser) {
     if (err) {
       return next(err);
     }
@@ -13,25 +14,46 @@ exports.getInventory = (req, res, next) => {
 };
 
 exports.updateInventory = (req, res, next) => {
-  User.findOne({ userId: req.userId }, function(err, existingUser) {
+  User.findOne({ _id: req.user._id }, function(err, existingUser) {
     if (err) {
       return next(err);
     }
 
     if (existingUser) {
-      const inventory = existingUser.inventory;
+      existingUser.inventory = req.body.user.inventory;
     }
+    existingUser
+      .save()
+      .then(() => res.json(existingUser.inventory))
+      .catch(err => res.status(400).json("Error: " + err));
   });
 };
 
 exports.makeMeal = (req, res, next) => {
-  User.findOne({ userId: req.userId }, function(err, existingUser) {
+  Recipe.findOne({ _id: "5dda03ea66b6edfd412bc899" }, function(
+    err,
+    existingUser
+  ) {
+    if (err) {
+      return next(err);
+    }
+    console.log(existingUser);
+
+    if (existingUser) {
+      res.json({ index: req.body.index });
+    }
+  });
+};
+
+// TODO: Remove
+exports.fakeRoute = (req, res, next) => {
+  Recipe.findOne({ _id: "5dda03ea66b6edfd412bc899" }, function(err, recipe) {
     if (err) {
       return next(err);
     }
 
-    if (existingUser) {
-      const inventory = existingUser.inventory;
+    if (recipe) {
+      res.json({ recipe });
     }
   });
 };
--- models/recipe.js
+++ models/recipe.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
 
-const recipeSchema = mongoose.Schema({
-  // TODO: Group by allergens
-  index: Number,
-  prep_time: Number,
-  review_count: Number,
-  description: String,
-  ingredients: [String],
-  total_time: Number,
-  ratings: Number,
-  cook_time: Number,
-  author: String,
-  name: String,
-  photo_url: String,
-  instructions: String,
-  ingredient_count: Number
-});
+const recipeSchema = mongoose.Schema(
+  {
+    // TODO: Group by allergens
+    index: Number,
+    prep_time: Number,
+    review_count: Number,
+    description: String,
+    ingredients: String,
+    total_time: Number,
+    ratings: Number,
+    cook_time: Number,
+    author: String,
+    name: String,
+    photo_url: String,
+    instructions: String,
+    ingredient_count: String
+  },
+  { collection: "recipe" }
+);
 
-module.exports = mongoose.model("recipe", recipeSchema);
+module.exports = mongoose.model("recipe", recipeSchema, "recipe");
