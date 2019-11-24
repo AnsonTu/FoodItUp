581f9bc401ea01d9edaf038a7df4a93dc9115302
Anson Tu <anson9137@gmail.com>
2019-11-23 23:19:17 -0500

    Updated recipe model and .env file

--- models/recipe.js
+++ models/recipe.js
@@ -6,13 +6,15 @@ const recipeSchema = mongoose.Schema({
   prep_time: Number,
   review_count: Number,
   description: String,
+  ingredients: [String],
   total_time: Number,
   ratings: Number,
   cook_time: Number,
   author: String,
   name: String,
   photo_url: String,
-  instructions: String
+  instructions: String,
+  ingredient_count: Number
 });
 
 module.exports = mongoose.model("recipe", recipeSchema);
